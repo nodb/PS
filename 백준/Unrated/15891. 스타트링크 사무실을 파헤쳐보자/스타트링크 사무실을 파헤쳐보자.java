f27883befe07a401446b43de3524363e8215a2a2
Noh Da-Bin <scnoh02@naver.com>
2025-02-18 16:10:17 +0900

    [Unrated] Title: 스타트링크 사무실을 파헤쳐보자, Time: 100 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Unrated/15891. 스타트링크 사무실을 파헤쳐보자/스타트링크 사무실을 파헤쳐보자.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = {65, 17, 4, 4, 64};
+		System.out.println(arr[n-1]);
+	}
+}
+
