e0ba53940a3c38bd765d5d78c41bc7fb4ede5640
Noh Da-Bin <scnoh02@naver.com>
2025-03-03 15:57:48 +0900

    [Bronze V] Title: Shares, Time: 100 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+            int N = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            System.out.println(S / (N + 1));
+        }
+    }
+}
