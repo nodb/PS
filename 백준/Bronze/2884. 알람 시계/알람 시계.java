e44425ddd15d0fc7f166ede4bf721d21ab3cda41
Noh Da-Bin <scnoh02@naver.com>
2025-03-03 12:38:48 +0900

    [Bronze III] Title: 알람 시계, Time: 124 ms, Memory: 16076 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		m -= 45;
+		if (m < 0) {
+			m += 60;
+			h -= 1;
+		}
+		if (h < 0) {
+			h += 24;
+		}
+		System.out.println(h + " " + m);
+	}
+}
