ff329b0f2d1afbff24217b924df5f6ec55ddb324
Noh Da-Bin <scnoh02@naver.com>
2025-03-03 15:19:40 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+            for (int j = 0; j < N; j++) {
+                if (i != j && arr[j][0] > arr[i][0] && arr[j][1] > arr[i][1]) {
+                    rank++;
+                }
+            }
+            sb.append(rank + " ");
+        }
+        System.out.println(sb);
+    }
+}
