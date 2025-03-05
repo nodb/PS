2297371637017d02a29ecdf3d1e11296339fc103
Noh Da-Bin <scnoh02@naver.com>
2025-03-05 14:31:01 +0900

    [Silver II] Title: 최대 힙, Time: 316 ms, Memory: 27528 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num == 0) {
+				if (pq.size() == 0)
+					sb.append(0 + "\n");
+				else
+					sb.append(pq.poll() + "\n");
+			} else
+				pq.offer(num);
+		}
+		System.out.println(sb);
+	}
+}
