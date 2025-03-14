5b707383aef3f67483cbad3073018b835b7cf49e
Noh Da-Bin <scnoh02@naver.com>
2025-03-15 02:21:06 +0900

    [Bronze V] Title: A+B - 7, Time: 76 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			sb.append("Case #" + tc + ": ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			sb.append(A + B).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
