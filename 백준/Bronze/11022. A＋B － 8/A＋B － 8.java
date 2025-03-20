43471512b2c9de279d37166f4dc73f07faed5f34
Noh Da-Bin <scnoh02@naver.com>
2025-03-20 15:05:42 +0900

    [Bronze V] Title: A+B - 8, Time: 72 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int first = Integer.parseInt(st.nextToken());
+			int second = Integer.parseInt(st.nextToken());
+			System.out.println("Case #" + i + ": " + first + " + " + second + " = " + (first + second));
+		}
+	}
+}
