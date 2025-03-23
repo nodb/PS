39bb66e66591a846a580b89742351e064ca27bcc
Noh Da-Bin <scnoh02@naver.com>
2025-03-23 22:09:18 +0900

    [Gold IV] Title: 공유기 설치, Time: 224 ms, Memory: 28192 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -2,41 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int C = Integer.parseInt(st.nextToken());
 		int arr[] = new int[N];
+		int max = 0;
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			if (arr[i] > max)
+				max = arr[i];
 		}
 		Arrays.sort(arr);
-		
-		int minDis = 1;
-		int maxDis = arr[N - 1] - arr[0];
+		int min = 1;
 		int result = 0;
-		
-		while (minDis <= maxDis) {
-			int midDis = (minDis + maxDis) / 2; // 공유기간 거리
-//			System.out.println(Arrays.toString(arr));
-//			System.out.println(minDis + " " + maxDis + " " + midDis);
-			int install = arr[0]; // 설치 값
+		while (min <= max) {
+			int mid = (min + max) / 2;
 			int cnt = 1;
+			int select = arr[0];
 			for (int i = 1; i < N; i++) {
-				if (install + midDis <= arr[i]) {
-					install = arr[i];
+				if (select + mid <= arr[i] ) {
+					select = arr[i];
 					cnt++;
 				}
 			}
-//			System.out.println("cnt : " + cnt);
-		    if (cnt >= C) {
-		        result = midDis;     // 현재 midDis 값 저장
-		        minDis = midDis + 1; // 더 큰 간격을 시도
-		    } else {
-		        maxDis = midDis - 1; // 설치 불가능하므로 간격을 줄여야 함
-		    }
+			if (cnt >= C) {
+				result = mid;
+				min = mid + 1;
+			} else
+				max = mid - 1;
 		}
 		System.out.println(result);
 	}
