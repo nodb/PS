bbc11118a3613598749bdea09bb7943b0bd2449b
Noh Da-Bin <scnoh02@naver.com>
2025-03-31 15:23:24 +0900

    [Bronze V] Title: 사파리월드, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long n = Integer.parseInt(st.nextToken());
+		long m = Integer.parseInt(st.nextToken());
+		System.out.println(Math.abs(n-m));
+	}
+}
