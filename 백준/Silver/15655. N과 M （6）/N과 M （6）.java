2f2bae860d66b00fe94d3d6147b39acc88a9dbde
Noh Da-Bin <scnoh02@naver.com>
2025-04-01 10:53:41 +0900

    [Silver III] Title: N과 M (6), Time: 120 ms, Memory: 15872 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int arr[];
+    static int save[];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        save = new int[M];
+        nm(0, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void nm(int num, int cnt) {
+        if (cnt == M) {
+            for (int i : save) {
+                sb.append(i + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = num; i < N; i++) {
+            save[cnt] = arr[i];
+            nm(i + 1, ++cnt);
+            cnt--;
+        }
+    }
+}
