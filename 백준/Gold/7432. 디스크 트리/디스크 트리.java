a9a450c5a272b469ff6673714a872f9fae08089d
Noh Da-Bin <scnoh02@naver.com>
2025-04-10 17:47:52 +0900

    [Gold III] Title: 디스크 트리, Time: 200 ms, Memory: 24680 KB -BaekjoonHub

--- 백준/Gold/7432. 디스크 트리/디스크 트리.java
+++ 백준/Gold/7432. 디스크 트리/디스크 트리.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	private static StringBuilder sb;
+
 	static class Node {
 		String name;
 		TreeMap<String, Node> children;
@@ -14,15 +16,17 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
 
 		Node root = new Node("");
 
 		for (int i = 0; i < n; i++) {
 			String path = br.readLine();
-			String[] dirs = path.split("\\\\");
 			Node current = root;
-			for (String dir : dirs) {
+			StringTokenizer st = new StringTokenizer(path, "\\");
+			while (st.hasMoreTokens()) {
+				String dir = st.nextToken();
 				if (!current.children.containsKey(dir))
 					current.children.put(dir, new Node(dir));
 				current = current.children.get(dir);
@@ -31,13 +35,15 @@ public class Main {
 
 		for (Node node : root.children.values())
 			check(node, 0);
+
+		System.out.println(sb);
 	}
 
 	static void check(Node node, int depth) {
 		if (!node.name.equals("")) {
 			for (int i = 0; i < depth; i++)
-				System.out.print(" ");
-			System.out.println(node.name);
+				sb.append(" ");
+			sb.append(node.name).append("\n");
 		}
 
 		for (Node child : node.children.values())
