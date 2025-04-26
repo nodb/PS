5973be52de3455a1bd248cfb04aba732dd5eef16
Noh Da-Bin <scnoh02@naver.com>
2025-04-26 13:01:15 +0900

    [Bronze V] Title: Rook, Time: 60 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		System.out.println("  ___  ___  ___");
+		System.out.println("  | |__| |__| |");
+		System.out.println("  |           |");
+		System.out.println("   \\_________/");
+		System.out.println("    \\_______/");
+		System.out.println("     |     |");
+		System.out.println("     |     |");
+		System.out.println("     |     |");
+		System.out.println("     |     |");
+		System.out.println("     |_____|");
+		System.out.println("  __/       \\__");
+		System.out.println(" /             \\");
+		System.out.println("/_______________\\");
+	}
+}
