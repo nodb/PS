8fa36258d9f7ce5d0fbfdc9fd0a84c8981c72faf
Noh Da-Bin <scnoh02@naver.com>
2025-05-15 17:36:09 +0900

    [Bronze V] Title: 웰컴, Time: 60 ms, Memory: 11228 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		System.out.print(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
