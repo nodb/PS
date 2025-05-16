1e3028821962b64088450c97fa9706095f69a500
Noh Da-Bin <scnoh02@naver.com>
2025-05-16 17:39:56 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 56 ms, Memory: 11248 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");
+		System.out.println(sb);
+	}
+}
