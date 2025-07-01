28d9c9e8b478d35a8a932a8a5b9e3d052ba72bee
Noh Da-Bin <scnoh02@naver.com>
2025-07-01 19:02:01 +0900

    [Gold IV] Title: 돌 그룹, Time: 164 ms, Memory: 31868 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.java
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.java
@@ -2,92 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static HashSet<rock> hs = new HashSet<>();
-	static Queue<rock> q = new LinkedList<>();
-
-	public static class rock {
-		int a, b, c;
-
-		rock(int a, int b, int c) {
-			int gcd = gcd(a, gcd(b, c));
-			int arr[] = { a / gcd, b / gcd, c / gcd };
-			// 오름차순 정렬
-			Arrays.sort(arr);
-			this.a = arr[0];
-			this.b = arr[1];
-			this.c = arr[2];
-		}
-
-		// 최대공약수(GCD) - 유클리드 호제법
-		int gcd(int x, int y) {
-			while (y != 0) {
-				int temp = y;
-				y = x % y;
-				x = temp;
-			}
-			return x;
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			if (this == o)
-				return true;
-			if (o == null || getClass() != o.getClass())
-				return false;
-			rock other = (rock) o;
-			return a == other.a && b == other.b && c == other.c;
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(a, b, c);
-		}
-	}
+	static boolean[][] visited = new boolean[1501][1501]; // 세 번째 값 : sum - a - b
+	static Queue<int[]> q = new LinkedList<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int first[] = new int[3];
-		for (int i = 0; i < 3; i++)
-			first[i] = Integer.parseInt(st.nextToken());
 
-		check(first[0], first[1], first[2]);
-		// 1. 크기가 같지 않은 두 그룹
-		// 2. 작은 쪽 : X, 큰 쪽 : Y
-		// 3. X += X
-		// 4. Y -= X
+		// hashset 사용하지 않고 배열로 처리
+		int arr[] = new int[3];
+		arr[0] = Integer.parseInt(st.nextToken());
+		arr[1] = Integer.parseInt(st.nextToken());
+		arr[2] = Integer.parseInt(st.nextToken());
+
+        // sum은 항상 유지
+		int sum = arr[0] + arr[1] + arr[2];
+		if (sum % 3 != 0) {
+			System.out.println(0);
+			return;
+		}
+
+		Arrays.sort(arr); // 항상 오름차순 정렬
+		q.add(arr);
+		visited[arr[0]][arr[1]] = true;
 
 		// 모든 경우 다 보기
-		// - 중복 방지 -> Set
-		// - - Set에서 최소 공배수로 관리
-		// - 새로운 값이 Set에 없다면 Queue에 추가
-		// - 종료 조건 : [1, 2, 3] 배열
+		// - 중복 방지 -> visited
+		// - 방문한 적 없다면 Queue에 추가
 		while (!q.isEmpty()) {
-			rock r = q.poll();
-			if (r.a == 1 && r.b == 1 && r.c == 1) {
+			int now[] = q.poll();
+            
+            // 종료 조건 : 배열의 세 값이 동일
+			if (now[0] == now[1] && now[1] == now[2]) {
 				System.out.println(1);
 				return;
 			}
-			if (r.a != r.b)
-				check(2 * r.a, r.b - r.a, r.c);
-			if (r.b != r.c)
-				check(r.a, 2 * r.b, r.c - r.b);
-			if (r.a != r.c)
-				check(2 * r.a, r.b, r.c - r.a);
+            
+            // 1. 크기가 같지 않은 두 그룹
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					if (i == j)
+						continue;
+                    // 2. 작은 쪽 : X, 큰 쪽 : Y
+					if (now[i] < now[j]) { // X = now[i], Y = now[j]
+						int[] next = now.clone();
+						next[i] += now[i]; // X += X
+						next[j] -= now[i]; // Y -= X
+						Arrays.sort(next); // 항상 오름차순 정렬
+						if (!visited[next[0]][next[1]]) {
+							visited[next[0]][next[1]] = true;
+							q.add(next);
+						}
+					}
+				}
+			}
 		}
 		System.out.println(0);
 	}
-
-	static void check(int i, int j, int k) {
-		rock r = new rock(i, j, k);
-		if (!hs.contains(r)) {
-			hs.add(r);
-			q.add(r);
-		}
-	}
 }
-
-//A = X + X
-//A = Y - X
-//X + X = Y - X
-//Y = 3X
