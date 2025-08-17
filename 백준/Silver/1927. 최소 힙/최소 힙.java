9f0390a46c134684abacc5852e956570a551253f
Noh Da-Bin <scnoh02@naver.com>
2025-08-18 00:18:15 +0900

    [Silver II] Title: 최소 힙, Time: 1072 ms, Memory: 32732 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -2,22 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
 		int N = Integer.parseInt(br.readLine());
+
+		// 우선순위 큐 사용
+		// - 가장 작은 값을 출력을 위해!
+		// - 큐(Queue) : 먼저 들어오는 데이터가 먼저 나가는 자료구조, FIFO(First In First Out)
+		// - 우선순위 큐(Priority Queue) : 우선순위가 높은 데이터가 먼저 나가는 자료구조
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+		// N번 입력 받기
 		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(br.readLine());
-			if (num == 0) {
+			int x = Integer.parseInt(br.readLine());
+			if (x == 0) {
 				if (pq.size() == 0)
-					sb.append(0 + "\n");
+					System.out.println(0);
 				else
-					sb.append(pq.poll() + "\n");
-			} else
-				pq.offer(num);
+					System.out.println(pq.remove());
+			} else {
+				pq.add(x);
+			}
 		}
-		System.out.println(sb);
 	}
 }
