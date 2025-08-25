0a82719310d68a8e13065d0b050e3bb79f21449d
Noh Da-Bin <scnoh02@naver.com>
2025-08-25 09:31:22 +0900

    [Silver I] Title: 절댓값 힙, Time: 260 ms, Memory: 24656 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,28 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> U = new PriorityQueue<>(); // 양의 정수
-		PriorityQueue<Integer> D = new PriorityQueue<>(Collections.reverseOrder()); // 음의 정수
+		PriorityQueue<Integer> minH = new PriorityQueue<>(Collections.reverseOrder());
+		PriorityQueue<Integer> maxH = new PriorityQueue<>();
 
 		for (int i = 0; i < N; i++) {
 			int x = Integer.parseInt(br.readLine());
-			if (x > 0) {
-				U.add(x);
+			if (x == 0) {
+				int min = 0;
+				int max = 0;
+				if (!minH.isEmpty())
+					min = minH.remove();
+				if (!maxH.isEmpty())
+					max = maxH.remove();
+
+				if (min == 0 && max == 0)
+					sb.append(0).append("\n");
+				else if (min == 0 && max != 0)
+					sb.append(max).append("\n");
+				else if (min != 0 && max == 0)
+					sb.append(min).append("\n");
+				else { // min != 0 && max != 0
+					if (min * -1 <= max) {
+						sb.append(min).append("\n");
+						maxH.add(max);
+					} else {
+						sb.append(max).append("\n");
+						minH.add(min);
+					}
+				}
+
 			} else if (x < 0) {
-				D.add(x);
-			} else { // x == 0
-				if (U.isEmpty() && D.isEmpty())
-					System.out.println(0);
-				else if (U.isEmpty())
-					System.out.println(D.poll());
-				else if (D.isEmpty())
-					System.out.println(U.poll());
-				else
-					System.out.println(D.peek() * -1 <= U.peek() ? D.poll() : U.poll());
+				minH.add(x);
+			} else if (x > 0) {
+				maxH.add(x);
 			}
 		}
+		System.out.println(sb);
 	}
 }
