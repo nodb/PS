186db51c404ee8349fdb6afdb4a221a87ce13607
Noh Da-Bin <scnoh02@naver.com>
2025-10-21 21:59:18 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 68 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -2,34 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int T = Integer.parseInt(br.readLine());
-        
-        // (해당 종류의 의상 개수 + 1)을 곱한 후, 아무것도 입지 않는 경우(알몸)를 제외하는 방식
-        for (int tc = 0; tc < T; tc++) {
-            int n = Integer.parseInt(br.readLine());
-            HashMap<String, Integer> hm = new HashMap<>();
-            
-            for (int i = 0; i < n; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                st.nextToken();
-                String type = st.nextToken();
-                hm.put(type, hm.getOrDefault(type, 0) + 1);
-            }
-            
-            int result = 1;
-            for (int count : hm.values()) {
-                // count + 1은 해당 종류에서 한 개의 의상 선택 또는 선택하지 않는 경우의 수
-                result *= (count + 1);
-            }
-            
-            // 알몸인 경우(아무것도 선택하지 않은 경우)를 빼줌
-            sb.append(result - 1).append("\n");
-        }
-        
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			Map<String, Integer> m = new HashMap<>();
+			for (int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				st.nextToken();
+				String s = st.nextToken();
+				int cnt = 0;
+				if (m.containsKey(s)) {
+					cnt = m.get(s);
+				}
+				m.put(s, cnt + 1);
+			}
+			int result = 1; // 출력 값
+			for (int num : m.values()) {
+				result *= num + 1;
+			}
+			sb.append(result - 1).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
