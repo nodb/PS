235308b255ae41265a1b13f9daf38e4392e9e8c5
Noh Da-Bin <scnoh02@naver.com>
2025-10-24 17:39:20 +0900

    [Silver III] Title: 파도반 수열, Time: 68 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -3,37 +3,22 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
 
-		int T = Integer.parseInt(br.readLine());
+			long[] arr = new long[101];
 
-		int[] list = new int[T];
-		int max = 0;
-		for (int i = 0; i < T; i++) {
-			list[i] = Integer.parseInt(br.readLine());
-			if (list[i] > max) {
-				max = list[i];
-			}
-		}
-
-		long[] arr = new long[max + 1];
-
-		if (max >= 1)
 			arr[1] = 1;
-		if (max >= 2)
 			arr[2] = 1;
-		if (max >= 3)
 			arr[3] = 1;
-
-		for (int i = 4; i <= max; i++) {
-			arr[i] = arr[i - 2] + arr[i - 3];
+			for (int i = 4; i <= 100; i++) {
+				arr[i] = arr[i - 3] + arr[i - 2];
+			}
+			sb.append(arr[n] + "\n");
 		}
-
-		for (int n : list) {
-			sb.append(arr[n]).append("\n");
-		}
-
-		System.out.print(sb);
+		System.out.println(sb);
 	}
 }
