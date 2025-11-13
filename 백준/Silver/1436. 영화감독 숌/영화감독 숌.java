0cf40718c9266ec0f3784859ebd65fd6d9c7024d
Noh Da-Bin <scnoh02@naver.com>
2025-11-13 22:11:07 +0900

    [Silver V] Title: 영화감독 숌, Time: 280 ms, Memory: 114816 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,19 +1,19 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-
+		int n = Integer.parseInt(br.readLine());
 		int cnt = 0;
-		int num = 666;
-
-		while (true) {
-			if (String.valueOf(num++).contains("666")) {
-				if (++cnt == N)
-					break;
+		for (int i = 0; i < Integer.MAX_VALUE; i++) {
+			if ((i + "").contains("666")) {
+				cnt++;
+			}
+			if (cnt == n) {
+				System.out.println(i);
+				return;
 			}
 		}
-		System.out.println(--num);
 	}
 }
